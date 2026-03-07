5fa6f0ca3f1fb0bb159733c89b54e9491023ab08
YashTiwari-2330 <yashtiwari2330@gmail.com>
2026-03-07 15:02:31 +0530

    Ai-SaaS based project

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardPage() {
+  return ( 
+    <p>This is Dashboard Page(Protected)</p>
+  );
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,6 @@
+
+export default function LandingPage() {
+  return ( 
+    <p>This is our landing page</p>
+  );
+}
